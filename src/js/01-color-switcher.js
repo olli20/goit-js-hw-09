e710ed83685909e3d8280d5dcc3e1dbd0bf5a7b9
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-11-29 18:25:05 +0100

    task 1 part 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let isStarted = false;
+let intervalId = null;
+
+const handleStart = () => {
+    if (isStarted) {
+        return;
+    };
+
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    },
+    1000);
+    isStarted = true;
+};
+
+refs.startBtn.addEventListener('click', handleStart);
+
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    isStarted = false;
+});
