04adf50125c65286cf6fc730310eba502fbd0bf1
Olena Voina <105532756+olli20@users.noreply.github.com>
2022-12-06 18:42:36 +0100

    try to fix error on gitpages

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,7 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
-  return promise = new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
@@ -10,6 +10,8 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
+
+  return promise;
 };
 
 const refs = {
