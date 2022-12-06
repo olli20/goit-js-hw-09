c7b3c87ae0a75a0d08b994f1fb7886d6bc48249c
olli20 <105532756+olli20@users.noreply.github.com>
2022-12-06 16:30:01 +0000

    Deploying to gh-pages from @ olli20/goit-js-hw-09@1282656785b106a8583caaad16cf86ddf51cb35a 🚀

---
+++ lection-promises.8c50d82a.js
@@ -0,0 +1,2 @@
+var e;(e="cake",new Promise(((o,n)=>{const c=Math.random()>.5;setTimeout((()=>{c&&o(`here you go: ${e}`),n("Enschuldigung, der Kuhlschrank ist leer")}),1e3)}))).then((e=>console.log(e))).catch((e=>console.log(e)));
+//# sourceMappingURL=lection-promises.8c50d82a.js.map
---
+++ lection-promises.e06c19e5.js
@@ -0,0 +1,2 @@
+!function(){var n;(n="cake",new Promise((function(o,e){var c=Math.random()>.5;setTimeout((function(){c&&o("here you go: ".concat(n)),e("Enschuldigung, der Kuhlschrank ist leer")}),1e3)}))).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}();
+//# sourceMappingURL=lection-promises.e06c19e5.js.map
